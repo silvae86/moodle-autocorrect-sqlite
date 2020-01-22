79cda474d72f0c089903f6a42cae7b1038c6fe11
João Rocha da Silva <joaorosilva@gmail.com>
2020-01-22 01:41:58 +0000

    Added sqlite3 installation commands
    Added sqlite3 installation commands

--- setup.sh
+++ setup.sh
@@ -14,11 +14,17 @@ if [ "$(uname)" == "Darwin" ]; then
 
 	# install python
 	brew install sashkab/python/python37
+
+	#install sqlite3
+	brew install sqlite3
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     # Do something under GNU/Linux platform
 
 	# install python
 	sudo apt-get install -y python3.7 python3.7-venv
+
+	#install sqlite3
+	sudo apt-get install -y sqlite3
 fi
 
 # install venv
